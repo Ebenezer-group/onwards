981d4da8252f07c375f2504e927112496d80f081
brian <woodbrian77@gmail.com>
2017-03-10 07:02:28 -0600

    Move more code from source files to headers

--- SendBufferCompressed.hh
+++ SendBufferCompressed.hh
@@ -1,7 +1,10 @@
 #pragma once
 
+#include "ErrorWords.hh"
+#include "IO.hh"
 #include "qlz_wrapper.hh"
 #include "SendBufferHeap.hh"
+#include <string.h>
 
 namespace cmw {
 
@@ -12,15 +15,51 @@ class SendBufferCompressed : public SendBufferHeap
   char* compressedBuf;
   qlz_compress_wrapper compress;
 
-  bool FlushFlush (::sockaddr*,::socklen_t);
+  bool FlushFlush (::sockaddr* toAddr,::socklen_t toLen)
+  {
+    int const bytes=sockWrite(sock_,compressedBuf,compIndex,toAddr,toLen);
+    if(bytes==compIndex){compIndex=0;return true;}
+
+    compIndex-=bytes;
+    ::memmove(compressedBuf,compressedBuf+bytes,compIndex);
+    return false;
+  }
 
 public:
-  SendBufferCompressed (int sz);
-  ~SendBufferCompressed ();
-  void Compress ();
-  bool Flush (::sockaddr* =nullptr,::socklen_t=0);
+  SendBufferCompressed (int sz) : SendBufferHeap(sz),compSize(sz+(sz>>3)+400)
+                                  ,compressedBuf(new char[compSize]) {}
+
+  ~SendBufferCompressed ()
+  { delete [] compressedBuf; }
+
+  void Compress ()
+  {
+    if(index+(index>>3)+400>compSize-compIndex)
+      throw failure("Not enough room in compressed buf");
+    compIndex+=::qlz_compress(buf,compressedBuf+compIndex
+                              ,index,compress.state_compress);
+    Reset();
+  }
+
+  bool Flush (::sockaddr* toAddr=nullptr,::socklen_t toLen=0)
+  {
+    bool rc=true;
+    bool haveWritten=false;
+    if(compIndex>0){
+      rc=FlushFlush(toAddr,toLen);
+      haveWritten=true;
+    }
+
+    if(index>0){
+      Compress();
+      if(!haveWritten)rc=FlushFlush(toAddr,toLen);
+      else rc=false;
+    }
+    return rc;
+  }
 
   inline void CompressedReset ()
   {compIndex=0;compress.reset();Reset();}
 };
 }
+
--- makefile
+++ makefile
@@ -13,7 +13,7 @@ subdirs:
 	$(MAKE) -C tiers
 	$(MAKE) -C example
 
-objects = quicklz.o marshalling_integer.o File.o IO.o SendBuffer.o SendBufferCompressed.o SendBufferFile.o
+objects = quicklz.o marshalling_integer.o File.o IO.o SendBuffer.o SendBufferFile.o
 libhome.a: $(objects)
 	ar r $@ $(objects)
 
