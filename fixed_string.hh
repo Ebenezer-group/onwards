cf61d6c931660b380fa324a305e97720cdd5967c
brian <brian@Unknown.domain>
2017-07-25 19:26:49 -0500

    Tweak

--- fixed_string.hh
+++ fixed_string.hh
@@ -22,7 +22,7 @@ class fixed_string{
 
   inline explicit fixed_string (::std::string_view s):length(s.length()){
     if(length()>N-1)throw failure("fixed_string ctor");
-    ::strncpy(&str[0],s.data(),length);
+    ::strncpy(&str[0],s.data(),length());
     str[length]='\0';
   }
 
