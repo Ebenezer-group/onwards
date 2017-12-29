a802fbc0b44593099847f6183c142b84ae6b4b62
brian <woodbrian77@gmail.com>
2017-12-29 16:35:53 -0600

    Correction for namespace

--- platforms.hh
+++ platforms.hh
@@ -1,10 +1,10 @@
 #pragma once
-namespace cmw{
 #if defined(_MSC_VER)||defined(WIN32)||defined(_WIN32)||defined(__WIN32__)||defined(__CYGWIN__)
 #define CMW_WINDOWS
 #include"ErrorWords.hh"
 #include<winsock2.h>
 #include<ws2tcpip.h>
+namespace cmw{
 using sock_type=SOCKET;
 using file_type=HANDLE;
 inline int GetError (){return WSAGetLastError();}
@@ -13,8 +13,10 @@ inline void windowsStart (){
   int rc=WSAStartup(MAKEWORD(2,2),&w);
   if(0!=rc)throw failure("WSAStartup:")<<rc;
 }
+}
 #else
 #include<errno.h>
+namespace cmw{
 using sock_type=int;
 using file_type=int;
 inline int GetError (){return errno;}
