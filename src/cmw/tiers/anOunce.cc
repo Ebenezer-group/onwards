e3fe02f79a030b7e57d7cc255511aa2667c25181
brian <woodbrian77@gmail.com>
2018-08-23 18:19:20 -0500

    Tweaks

--- src/cmw/tiers/anOunce.cc
+++
@@ -1,8 +0,0 @@
-  cmwRequest (ReceiveBuffer<R>& buf,::std::vector<cmwAccount> const& accts):
-       accountNbr(buf),path(buf){
-    bool found=false;
-    for(auto const& acct:accts){
-      if(acct.number==accountNbr){found=true;break;}
-    }
-    if(!found)throw failure("Invalid account number ")<<accountNbr();
-
