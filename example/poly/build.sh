f3a6749b59275b54ca5d1186a9e4bf61d8658883
brian <woodbrian77@gmail.com>
2019-03-17 05:43:38 -0500

    Update example

--- example/poly/build.sh
+++ example/poly/build.sh
@@ -1,3 +1,3 @@
-g++ -I. -I../.. -I/home/brian/boost_1_69_0/ -std=c++17 sendPoly.cc -o sendPoly
-g++ -I. -I../.. -I/home/brian/boost_1_69_0/ -std=c++17 receivePoly.cc -o receivePoly
+g++ -I. -I../../src -I/home/brian/boost_1_69_0/ -std=c++17 sendPoly.cc -o sendPoly
+g++ -I. -I../../src -I/home/brian/boost_1_69_0/ -std=c++17 receivePoly.cc -o receivePoly
 
