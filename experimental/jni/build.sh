6c26004f6aaeebb616abf5bd3be7aaa705353775
brian <woodbrian77@gmail.com>
2019-03-17 17:26:38 -0500

    Add a file

---
+++ experimental/jni/build.sh
@@ -0,0 +1,7 @@
+javac -h . jniGenz.java
+
+g++ -shared -fPIC -std=c++17 -o libnative.so -I ../../src -I /home/brian/android-studio/jre/include/ -I /home/brian/android-studio/jre/include/linux/ genzMain.cc 
+
+java -cp . -Djava.library.path=. jniGenz
+java -cp . -Djava.library.path=. jniGenz 2 ./middleBack.mdl 
+
