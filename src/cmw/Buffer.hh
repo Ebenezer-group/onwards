446dd1b5b7a6ce74e7f354dd07eb94b01db4be50
unknown <woodbrian77@gmail.com>
2019-08-07 23:46:46 -0500

    Change to receiveBlock

--- src/cmw/Buffer.hh
+++ src/cmw/Buffer.hh
@@ -566,7 +566,7 @@ void receiveBlock (SendBuffer& b,C<T,Ts...>const& c){
   ::int32_t n=c.size();
   b.receive(n);
   if constexpr(::std::is_arithmetic_v<T>){
-    if(n>0)b.receive(&*c.cbegin(),n*sizeof(T));
+    if(n>0)b.receive(c.data(),n*sizeof(T));
   }else if constexpr(::std::is_pointer_v<T>)for(auto e:c)e->marshal(b);
   else for(auto const& e:c)e.marshal(b);
 }
