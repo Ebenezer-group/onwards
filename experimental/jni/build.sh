7986c0eeea03b8846152933f0366bad43628a288
unknown <woodbrian77@gmail.com>
2019-11-11 10:20:19 -0600

    Update name

--- experimental/jni/build.sh
+++ experimental/jni/build.sh
@@ -3,5 +3,5 @@ javac -h . jniGenz.java
 g++ -shared -fPIC -std=c++17 -o libnative.so -I ../../src -I /home/brian/android-studio/jre/include/ -I /home/brian/android-studio/jre/include/linux/ genzMain.cc
 
 java -cp . -Djava.library.path=. jniGenz
-java -cp . -Djava.library.path=. jniGenz 2 ./middleBack.mdl
+java -cp . -Djava.library.path=. jniGenz 2 ./cmwA.mdl
 
