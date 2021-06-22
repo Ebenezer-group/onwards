944d3540715f04616210fc95a2ba8b5dd219bbdd
unknown <woodbrian77@gmail.com>
2021-06-22 08:59:53 -0700

    Testing CI

--- src/tiers/front/genz.cc
+++ src/tiers/front/genz.cc
@@ -15,7 +15,7 @@ int main (int ac,char **av)try{
   GetaddrinfoWrapper res(ac<4?"127.0.0.1":av[3],ac<5?"55555":av[4],SOCK_DGRAM);
   BufferStack<SameFormat> buf(res.getSock());
 
-  middle::marshal(buf,MarshallingInt(av[1]),av[2]);
+  ::middle::marshal(buf,MarshallingInt(av[1]),av[2]);
   for(int tm=8;tm<13;tm+=4){
     buf.send(res().ai_addr,res().ai_addrlen);
     setRcvTimeout(buf.sock_,tm);
