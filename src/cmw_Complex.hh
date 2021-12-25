4b742f41c2f68a33b49ddfc6264cefea313a6b8c
unknown <woodbrian77@gmail.com>
2021-12-24 20:13:45 -0600

    Changes to library for abbreviated function templates

--- src/cmw_Complex.hh
+++ src/cmw_Complex.hh
@@ -4,24 +4,23 @@
 #include<complex>
 
 namespace cmw{
-template<class T,class B>
-void complexMarshal (B& buf,::std::complex<T> const& c){
+void complexMarshal (auto& buf,auto const& c){
   buf.receive(c.real());
   buf.receive(c.imag());
 }
 
-template<class T,class B>
-::std::complex<T> complexGive (B& buf){
+template<class T>
+auto complexGive (auto& buf){
   //The following has an order of evaluation problem:
   //return ::std::complex<T>(give<T>(buf),give<T>(buf));
   T rl=give<T>(buf);
   return ::std::complex<T>(rl,give<T>(buf));
 }
 
-template<class C>int32_t marshalSegments (C&,SendBuffer&,uint8_t){return 0;}
+::int32_t marshalSegments (auto&,SendBuffer&,uint8_t){return 0;}
 
-template<class T,class... Ts,class C>
-::int32_t marshalSegments (C& c,SendBuffer& buf,uint8_t& segs){
+template<class T,class... Ts>
+::int32_t marshalSegments (auto& c,SendBuffer& buf,uint8_t& segs){
   ::int32_t n;
   if(c.template is_registered<T>()){
     n=c.template size<T>();
@@ -35,22 +34,21 @@ template<class T,class... Ts,class C>
   return n+marshalSegments<Ts...>(c,buf,segs);
 }
 
-template<class...Ts,class C>void marshalCollection (C& c,SendBuffer& buf){
+template<class...Ts>void marshalCollection (auto& c,SendBuffer& buf){
   auto const ind=buf.reserveBytes(1);
   ::uint8_t segs=0;
   if(c.size()!=marshalSegments<Ts...>(c,buf,segs))raise("marshalCollection");
   buf.receive(ind,segs);
 }
 
-template<class T,class C,class R>
-void buildSegment (C& c,ReceiveBuffer<R>& buf){
+template<class T>
+void buildSegment (auto& c,auto& buf){
   ::int32_t n=give<::uint32_t>(buf);
   c.template reserve<T>(n);
   for(;n>0;--n){c.template emplace<T>(buf);}
 }
 
-template<class C,class R,class F>
-void buildCollection (C& c,ReceiveBuffer<R>& buf,F f){
+void buildCollection (auto& c,auto& buf,auto f){
   for(auto segs=give<::uint8_t>(buf);segs>0;--segs){f(c,buf);}
 }
 
