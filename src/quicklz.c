4e55a01da1c3e72dcf27184887828ec5fd28bed4
Ebenezer-group <woodbrian77@gmail.com>
2023-08-25 22:08:45 -0600

    Remove unused qlz function

--- src/quicklz.c
+++ src/quicklz.c
@@ -131,26 +131,6 @@ struct qlz_state_decompress
 	#define CAST
 #endif
 
-int qlz_get_setting(int setting)
-{
-	switch (setting)
-	{
-		case 0: return QLZ_COMPRESSION_LEVEL;
-		case 1: return sizeof(qlz_state_compress);
-		case 2: return sizeof(struct qlz_state_decompress);
-		case 3: return QLZ_STREAMING_BUFFER;
-#ifdef QLZ_MEMORY_SAFE
-		case 6: return 1;
-#else
-		case 6: return 0;
-#endif
-		case 7: return QLZ_VERSION_MAJOR;
-		case 8: return QLZ_VERSION_MINOR;
-		case 9: return QLZ_VERSION_REVISION;
-	}
-	return -1;
-}
-
 #if QLZ_COMPRESSION_LEVEL == 1
 static int same(const unsigned char *src, size_t n)
 {
