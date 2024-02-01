3f5285bd89396450098153a516be00869ca3f32a
Ebenezer-group <woodbrian77@gmail.com>
2024-02-01 13:37:31 -0600

    Stop using Getaddrinfo in front tier

--- src/tiers/front/genz.cc
+++ src/tiers/front/genz.cc
@@ -12,12 +12,12 @@ int main (int ac,char** av)try{
   if(ac<3||ac>5)
     leave("Usage: genz account-num mdl-file-path [node] [port]\n");
   winStart();
-  GetaddrinfoWrapper res(ac<4?"127.0.0.1":av[3],ac<5?"55555":av[4],SOCK_DGRAM);
-  BufferStack<SameFormat> buf(res.getSock());
+  SockaddrWrapper sa(ac<4?"127.0.0.1":av[3],ac<5?55555:fromChars(av[4]));
+  BufferStack<SameFormat> buf(::socket(AF_INET,SOCK_DGRAM,IPPROTO_UDP));
 
   ::middle::marshal<udpPacketMax>(buf,MarshallingInt(av[1]),av[2]);
   for(int tm=8;tm<13;tm+=4){
-    buf.send(res().ai_addr,res().ai_addrlen);
+    buf.send((::sockaddr*)&sa,sizeof(sa));
     setRcvTimeout(buf.sock_,tm);
     if(buf.getPacket()){
       if(giveBool(buf))::std::exit(EXIT_SUCCESS);
