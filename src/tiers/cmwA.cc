5b2a6bcf06dcbe61a616de62a2d46d3a52fb48ab
Ebenezer-group <woodbrian77@gmail.com>
2024-11-05 15:11:20 -0600

    Tweak to ambassador

--- src/tiers/cmwA.cc
+++ src/tiers/cmwA.cc
@@ -31,12 +31,11 @@ class ioUring{
   constexpr static int maxBatch=10;
 
   auto getSqe (){
-    if(auto e=::io_uring_get_sqe(&rng);e)return e;
-    else{
-      ::io_uring_submit(&rng);
-      if((e=::io_uring_get_sqe(&rng)))return e;
-      raise("getSqe");
-    }
+    auto e=::io_uring_get_sqe(&rng);
+    if(e)return e;
+    ::io_uring_submit(&rng);
+    if((e=::io_uring_get_sqe(&rng)))return e;
+    raise("getSqe");
   }
 
  public:
