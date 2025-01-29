9dac5c70489beab2d4072fb2db897077e9ff59c9
Ebenezer-group <woodbrian77@gmail.com>
2025-01-28 21:35:25 -0600

    Tweaks to ambassador

--- src/tiers/cmwA.cc
+++ src/tiers/cmwA.cc
@@ -47,12 +47,12 @@ class ioUring{
   }
 
   ioUring (int sock,auto sp):iov{sp.data(),sp.size()},udpSock(sock){
+    auto bff=::mmap(0,103000,PROT_READ|PROT_WRITE,MAP_PRIVATE|MAP_ANONYMOUS,-1,0);
+    if(MAP_FAILED==bff)raise("mmap",errno);
     ::io_uring_params ps{};
     ps.flags=IORING_SETUP_SINGLE_ISSUER|IORING_SETUP_DEFER_TASKRUN;
     ps.flags|=IORING_SETUP_NO_MMAP|IORING_SETUP_NO_SQARRAY|IORING_SETUP_REGISTERED_FD_ONLY;
-    auto bff=::mmap(0,105000,PROT_READ|PROT_WRITE, MAP_PRIVATE|MAP_ANONYMOUS,-1,0);
-    if(MAP_FAILED==bff)raise("mmap",errno);
-    if(int rc=::io_uring_queue_init_mem(1024,&rng,&ps,bff,105000);rc<0)
+    if(int rc=::io_uring_queue_init_mem(1024,&rng,&ps,bff,103000);rc<0)
       raise("ioUring",rc);
     int regfds[]={sock,0};
     if(::io_uring_register_files(&rng,regfds,2))raise("io reg");
